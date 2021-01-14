ff2ca0d18529d5704a251a1af924726bc2daacc1
Shreyes <shreyessrivastava@gmail.com>
2021-01-14 17:56:31 +0530

    Post Reducer, Action and Initial Component

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 import EditProfile from './components/profile-forms/EditProfile';
 
 import PrivateRoute from './components/routing/PrivateRoute';
@@ -66,6 +67,7 @@ const App = () => {
                 path='/add-education'
                 component={AddEducation}
               />
+              <PrivateRoute exact path='/posts' component={Posts} />
             </Switch>
           </section>
         </Fragment>
