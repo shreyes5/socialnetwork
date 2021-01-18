707b45b1c4c6540cb9a9020f7f6d2eb5800b604a
Shreyes <shreyessrivastava@gmail.com>
2021-01-18 20:23:47 +0530

    Single post display but some issue in post/Posts idk what exactly but figure that out soon

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -3,6 +3,7 @@ import { setAlert } from './alert';
 import {
   ADD_POST,
   DELETE_POST,
+  GET_POST,
   GET_POSTS,
   POST_ERROR,
   UPDATE_LIKES,
@@ -110,6 +111,26 @@ export const addPost = (formData) => async (dispatch) => {
     });
 
     dispatch(setAlert('Post Created', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status,
+      },
+    });
+  }
+};
+
+//Get post
+export const getPost = (id) => async (dispatch) => {
+  try {
+    const res = await axios.get(`api/posts/${id}`);
+
+    dispatch({
+      type: GET_POST,
+      payload: res.data,
+    });
   } catch (err) {
     dispatch({
       type: POST_ERROR,
